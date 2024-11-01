31d89feda4da44db84270e9221a46d58e824d69f
Hanna <annpetlitskaya@gmail.com>
2024-11-01 09:54:37 +0100

    added assert_status_401 method, updated assert messages

--- classes/base_endpoint.py
+++ classes/base_endpoint.py
@@ -7,33 +7,36 @@ class Endpoint:
     json = None
 
     def assert_status_200(self):
-        assert self.response.status_code == 200
+        assert self.response.status_code == 200, f'AR is {self.response.status_code}, ER 200'
 
     def assert_status_400(self):
-        assert self.response.status_code == 400
+        assert self.response.status_code == 400, f'AR is {self.response.status_code}, ER 400'
 
     def assert_status_404(self):
-        assert self.response.status_code == 404
+        assert self.response.status_code == 404, f'AR is {self.response.status_code}, ER 404'
 
-    @staticmethod
-    def assert_status_405(meme_id, body, token):
-        return requests.post(f'{Endpoint.base_url}/meme/{meme_id}', json=body,
-                             headers={"Authorization": f"{token}"}).status_code == 405
+    def assert_status_405_with_invalid_http_method(self, meme_id, body, token):
+        self.response = requests.post(f'{Endpoint.base_url}/meme/{meme_id}', json=body, headers={
+            "Authorization": f"{token}"})
+        assert self.response.status_code == 405, f'AR is {self.response.status_code}, ER 405'
 
-    def assert_response_len(self):
-        return len(self.json) == 6, f'actual response id is {len(self.json)}, expected 6'
+    def assert_status_401(self):
+        assert self.response.status_code == 401, f'AR is {self.response.status_code}, ER 401'
+
+    def assert_response_len(self, length):
+        return len(self.json) == length, f'AR is {len(self.json)}, ER {length}'
 
     def assert_response_tags(self, tags):
-        assert self.json['tags'] == tags, f'actual response id is {self.json['tags']}, expected {tags}'
+        assert self.json['tags'] == tags, f'AR is {self.json['tags']}, ER {tags}'
 
     def assert_response_text(self, text):
-        assert self.json['text'] == text, f'actual response id is {self.json['text']}, expected {text}'
+        assert self.json['text'] == text, f'AR is {self.json['text']}, ER {text}'
 
     def assert_response_url(self, url):
-        assert self.json['url'] == url, f'actual response id is {self.json['url']}, expected {url}'
+        assert self.json['url'] == url, f'AR is {self.json['url']}, ER {url}'
 
     def assert_response_info(self, info):
-        assert self.json['info'] == info, f'actual response id is {self.json['info']}, expected {info}'
+        assert self.json['info'] == info, f'AR is {self.json['info']}, ER {info}'
 
     def assert_response_id(self, meme_id):
-        assert int(self.json['id']) == int(meme_id), f'actual response id is {self.json['id']}, expected {meme_id}'
+        assert int(self.json['id']) == int(meme_id), f'AR is {self.json['id']}, ER {meme_id}'
